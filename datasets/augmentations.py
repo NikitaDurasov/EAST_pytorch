b4112b97227fa11e34cf94646fef7713d9645c1e
Никита Дурасов <ndurasov@localhost.localdomain>
2019-04-27 13:30:50 +0000

    minor fixes in losses and training procedure

--- datasets/augmentations.py
+++ datasets/augmentations.py
@@ -8,7 +8,7 @@ class RBOX:
         self.scale = scale
 
     def __call__(self, sample):
-        image, bboxes = sample['image'], sample['bboxes']
+        image, bboxes = sample['image'], sample['bbox']
 
         bbox_map = np.zeros(image.shape[:2])
         dist_map = np.zeros(list(image.shape[:2]) + [2])
@@ -27,14 +27,13 @@ class RBOX:
                 "bbox_map": bbox_map,
                 "distances_map": dist_map}
 
-
 class QUAD:
 
     def __init__(self, scale=0.15):
         self.scale = scale
 
     def __call__(self, sample):
-        image, bboxes = sample['image'], sample['bboxes']
+        image, bboxes = sample['image'], sample['bbox']
 
         bbox_map = np.zeros(image.shape[:2])
         quad_formatting = np.zeros(list(image.shape[:2]) + [8])
@@ -51,3 +50,47 @@ class QUAD:
         return {"image": image,
                 "bbox_map": bbox_map,
                 "quad_formatting": quad_formatting}
+
+    
+class RandomCrop:
+    
+    def __init__(self, crop_size=(512, 512)):
+        self.crop_size = crop_size 
+        
+    def __call__(self, sample):
+        image = sample['image']
+        bbox_map = sample['bbox_map'] 
+        quad_formatting = sample['quad_formatting']
+        
+        new_x_b = np.random.randint(0, image.shape[1] - self.crop_size[1])
+        new_y_b = np.random.randint(0, image.shape[0] - self.crop_size[0])
+        
+        new_x_e = new_x_b + self.crop_size[1]
+        new_y_e = new_y_b + self.crop_size[0]
+            
+        new_sample = {"image": image[new_y_b:new_y_e, new_x_b:new_x_e, :],
+                      "bbox_map": bbox_map[new_y_b:new_y_e, new_x_b:new_x_e],
+                      "quad_formatting": quad_formatting[new_y_b:new_y_e, new_x_b:new_x_e, :]}
+        
+        return new_sample
+    
+class Normalize:
+    
+    def __init__(self):
+        self.imagenet_stats = {'mean': np.array([0.485, 0.456, 0.406]),
+                               'std': np.array([0.229, 0.224, 0.225])}
+    
+    def __call__(self, sample):
+        image = sample['image'].astype('float32') / 255
+        bbox_map = sample['bbox_map'] 
+        quad_formatting = sample['quad_formatting']
+        
+        image -= self.imagenet_stats['mean'].reshape(1, 1, 3)
+        image /= self.imagenet_stats['std'].reshape(1, 1, 3)
+        
+        new_sample = {"image": image,
+                      "bbox_map": bbox_map,
+                      "quad_formatting": quad_formatting}
+        
+        return new_sample
+        
--- train.py
+++ train.py
@@ -1,14 +1,70 @@
 import model
 import datasets
+import losses
+
+
+from dstorch.detection.icdar2015textloc import ICDAR2015TEXTLOC
+
 import dstorch
+import torch
+import torchvision
+
+dataset_path = '/srv/hd1/data/ndurasov/datasets/icdar2015/text_localization/'
 
 class Trainer():
 
     def __init__(self):
+        
+        # model creation
         self.model = model.east.EAST()
+        
+        # transform obj for dataset 
+        trans = torchvision.transforms.Compose([datasets.augmentations.QUAD(),
+                                        datasets.augmentations.RandomCrop((512, 512)),
+                                        datasets.augmentations.Normalize()])
+        
+        # datasets object 
+        self.dataset_train = ICDAR2015TEXTLOC(dp, split='train',
+                                              transform=trans,
+                                              transform_type='dict')
+        
+        self.dataset_test = ICDAR2015TEXTLOC(dp, split='test',
+                                             transform=trans,
+                                             transform_type='dict')
+        
+        # dataloaders objects
+        def collate_fn(batch):
+
+            images = list()
+            bbox_map = list()
+            quad_formatting = list()
+
+            for b in batch:
+                images.append(torch.from_numpy(b['image']))
+                bbox_map.append(b['bbox_map'])
+                quad_formatting.append(b['quad_formatting'])
+
+            images = torch.stack(images, dim=0)
+
+            return {"image": images, 
+                    "bbox_map": bbox_map, 
+                    "quad_formatting": quad_formatting}
+
+        train_dataloader = torch.utils.data.DataLoader(self.dataset_train, 
+                                                       batch_size=16,
+                                                       collate_fn=collate_fn,
+                                                       shuffle=True)
+        
+        self.test_dataloader = torch.utils.data.DataLoader(self.dataset_test, 
+                                                           batch_size=16,
+                                                           collate_fn=collate_fn)
 
     def train_one_epoch(self):
-        pass
+        
+        for batch in self.train_dataloader:
+            images, bbox_map, quad_formatting = batch['image'], batch['bbox_map'], batch['quad_formatting'] 
+            images = images.permute(0, 3, 1, 2)
+            bboxes_predictions = self.model(images)
 
     def validate_one_epoch(self):
         pass
